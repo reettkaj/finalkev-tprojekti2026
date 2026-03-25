700d29abe7c99b8e4adfbafed467a1b77091be72
monarkier <142782233+monarkier@users.noreply.github.com>
2026-03-25 15:17:33 +0200

    poistettu ylimääräiset tiedostot

--- db/create-user-sample.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'healthdiary'@'localhost' IDENTIFIED BY 'salakala';
-GRANT ALL PRIVILEGES ON `HealthDiary`.* TO 'healthdiary'@'localhost';
-FLUSH PRIVILEGES;
--- public/script.js
+++
@@ -1,1 +0,0 @@
-console.log('tämä on client-puolen javascriptia');
